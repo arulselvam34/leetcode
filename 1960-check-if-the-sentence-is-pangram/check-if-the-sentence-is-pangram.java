0c5627b7bda7b962e3ef48e66da6f585fdfcba9d
arulselvam34 <arulselvamdevaraj@gmail.com>
2024-08-20 18:21:10 +0530

    Time: 2 ms (76.01%) | Memory: 42 MB (7.09%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         boolean[] ch = new boolean[26];
-        for(int i=0;i<sentence.length();i++) {
-            ch[sentence.charAt(i)-97] = true;
+        for (int i = 0; i < sentence.length(); i++) {
+            ch[sentence.charAt(i) - 97] = true;
         }
 
-        if(present(ch)==true) {
+        if (present(ch) == true) {
             return true;
         }
         return false;
     }
+
     static boolean present(boolean[] ch) {
-        for(int i=0;i<ch.length;i++) {
-            if(ch[i]==false) {
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == false) {
                 return false;
             }
         }
