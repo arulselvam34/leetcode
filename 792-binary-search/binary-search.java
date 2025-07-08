2ef042dfe0ea9488c8d8d299bba3185f1844f254
Arulselvam D <73772218105@ksrct.net>
2025-07-08 16:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (60.55%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,24 +4,19 @@ class Solution {
         int high = nums.length - 1;
 
         while (low <= high) {
-            // Finding the mid using integer division
             int mid = low + (high - low) / 2;
 
-            // Target value is present at the middle of the array
             if (nums[mid] == target) {
                 return mid;
             }
-            // Target value is present in the low subarray
             else if (target < nums[mid]) {
                 high = mid - 1;
             }
-            // Target value is present in the high subarray
             else if (target > nums[mid]) {
                 low = mid + 1;
             }
         }
 
-        // Target value is not present in the array
         return -1; 
     }
 }
